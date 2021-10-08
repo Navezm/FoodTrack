78d5ad0f0c2bef01652fb5dfcecaa83b6a639956
Oldberamot <daune.arnaud@gmail.com>
2021-10-08 11:04:32 +0200

    initial commit

---
+++ src/main/java/be/TechnofuturTIC/foodtrack/FoodTrackApplication.java
@@ -0,0 +1,13 @@
+package be.TechnofuturTIC.foodtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodTrackApplication.class, args);
+    }
+
+}
